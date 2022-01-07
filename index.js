ac9cfe2612e5c6adce92d0e9e39a1e5753de3d02
born3am <psmaranhao@gmail.com>
2022-01-07 09:32:37 +0100

    update

--- index.js
+++ index.js
@@ -4,13 +4,28 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+// 1) all movies troute
 app.get('/movies', (req, res) => {
-  res.send('Movies')
+  res.send('Movies!')
 })
 
-app.get('/tvshows', (req, res) => {
-    res.send('TV-SHOWS')
-  })
+app.get("/movies", movies);
+
+
+
+// 2) new movies route
+
+
+// 3) action movies route
+
+
+
+// 4) find movies by id
+
+
+// app.get('/tvshows', (req, res) => {
+//     res.send('TV-SHOWS')
+//   })
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
